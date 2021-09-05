81e6c79ca7e2f0bb60a80e3675ef121ee9f16dd6
fimran2011@gmail.com <fimran2011@gmail.com>
2021-09-05 07:57:45 +0600

    fakestoreapi

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is heading one</h1>
+      <p>This is heading two</p>
+    </div>
+  );
+}
+
+export default App;
